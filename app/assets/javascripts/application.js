cf8d6d686ae04db3aa690dc325117ff8a4e368f8
srini <srinivas.valekar@gmail.com>
2014-03-16 22:37:10 +0530

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,20 +43,7 @@
 
 
 
-
-//= require_directory ./client/MainModuleJS
-//= require_directory ./client/controllersJS
-//= require_directory ./client/directivesJS
-//= require_directory ./client/servicesJS
-//= require masonry
-//= require places
-//= require posts
-//= require workplaces
-
-
-
-
-
+//=require_directory minifiedProduction
 
 
 
